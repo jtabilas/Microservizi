8366118adfedf03ed6e6dd2e6ef373ff16c7ce69
Patrick <94932581+jtabilas@users.noreply.github.com>
2023-09-08 16:06:45 +0200

    add

---
+++ orderserver/src/main/java/com/example/orderserver/OrderserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserverApplication.class, args);
+    }
+
+}
